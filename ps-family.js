874642d1e4a578faa30106c432cbdca890941c88
Pawnesh Rai <pawnesh.k.rai@gmail.com>
2014-03-26 16:45:16 +0530

    member positioning added

--- ps-family.js
+++ ps-family.js
@@ -11,6 +11,14 @@ var memberName = '';
 var memberGender = '';
 var memberAge = '';
 var memberPic = '';
+var memberRelation = '';
+var familyTree = new Array();
+var memberId = 0;
+var selectedMember;// refrence to selected member
+var self = true;
+var memberSpace = 92;
+var memberWidth = 115;
+var memberHeight = 107;
 
 (function($) {
     $.fn.pk_family = function(options) {
@@ -23,9 +31,10 @@ var memberPic = '';
     }
 
     function init() {
-        addMemberButton();
+       // addMemberButton();
         addBreadingGround();
         createNewMemberForm();
+        displayFirstForm();
     }
 
     function createNewMemberForm() {
@@ -39,13 +48,12 @@ var memberPic = '';
         $('<tr>').html('<td><label>Name</label></td><td><input type="text" value="" id="pk-name"/></td>').appendTo(table);
         $('<tr>').html(' <td><label>Gender</label></td><td><select id="pk-gender"><option value="Male">Male</option><option value="Female">Female</option></select></td>').appendTo(table);
         $('<tr>').html('<td><label>Age</label></td><td><input type="text" value="" id="pk-age"></td>').appendTo(table);
-        $('<tr>').html(' <td><label>Relation</label></td><td><select id="pk-relation">\n\\n\
-        <option value="Self">Self</option>\n\
+        $('<tr>').html(' <td class="relations"><label>Relation</label></td><td class="relations"><select id="pk-relation">\n\\n\
 <option value="Mother">Mother</option>\n\
 <option value="Father">Father</option>\n\\n\
-<option value="Father">Sibling</option>\n\\n\
-<option value="Father">Child</option>\n\\n\
-<option value="Father">Spouse</option>\n\\n\
+<option value="Sibling">Sibling</option>\n\\n\
+<option value="Child">Child</option>\n\\n\
+<option value="Spouse">Spouse</option>\n\\n\
 </select></td>').appendTo(table);
         $('<tr>').html('<td><label>Upload Photo</label></td><td><input type="file" id="pk-picture"></td>').appendTo(table);
         var buttonSave = $('<input>').attr('type', 'button');
@@ -68,7 +76,7 @@ var memberPic = '';
         memberGender = $('#pk-gender').val();
         memberAge = $('#pk-age').val();
         memberPic = $('#pk-picture');
-        memberRelation = $('#pk-relation');
+        memberRelation = $('#pk-relation').val();
         // after saving
         addMember();
         closeForm();
@@ -87,15 +95,65 @@ var memberPic = '';
         $(member).click(displayForm);
         $(member).appendTo(rootDiv);
     }
-    function displayForm() {
+    function displayForm(input) {
+        if (input != null)
+            selectedMember = input;
+        self = false;
+        $('.relations').css('display', '');
+        $(newMemberForm).css('display', 'block');
+    }
+    function displayFirstForm() {
+        selectedMember = null;
+        self = true;
+        $('.relations').css('display', 'none');
         $(newMemberForm).css('display', 'block');
     }
     function addMember() {
         var div = $('<div>').attr('class', 'member');
+        var xposition = 500;
+        var yposition = 200;
+        if (selectedMember != null) {
+            if (memberRelation == 'Spouse') {
+                // spouse will be added in right
+                xposition = parseInt($(selectedMember).attr('data-x')) + memberWidth + memberSpace;
+                yposition = parseInt($(selectedMember).attr('data-y'));
+            }
+            // child will be added below
+            if (memberRelation == 'Child') {
+                xposition = parseInt($(selectedMember).attr('data-x')) - memberWidth - memberSpace;
+                yposition = parseInt($(selectedMember).attr('data-y')) + memberHeight + memberSpace;
+            }
+            // sibling will be added consicutivly just higer above to current position
+            if (memberRelation == 'Sibling') {
+                xposition = $(selectedMember).attr('data-x') + (memberWidth*2) + (memberSpace*2);
+                yposition = $(selectedMember).attr('data-y') - memberHeight - memberSpace;
+            }
+            // father will be added above left
+            if (memberRelation == 'Father') {
+                xposition = $(selectedMember).attr('data-x') - memberWidth - memberSpace;
+                yposition = $(selectedMember).attr('data-y') -(memberHeight*2) - (memberSpace*2);
+            }
+            // mother will be added above right
+            if (memberRelation == 'Mother') {
+                xposition = $(selectedMember).attr('data-x')  - memberWidth - memberSpace;
+                yposition = $(selectedMember).attr('data-y') - (memberHeight*2) - (memberSpace*2);
+                // from father prespective mother is wife
+                xposition = xposition + memberWidth + memberSpace;
+            }
+        }
+
         $(div).attr('data-name', memberName);
         $(div).attr('data-gender', memberGender);
         $(div).attr('data-age', memberAge);
         $(div).attr('data-relation', memberRelation);
+        $(div).attr('data-id', memberId);
+        $(div).attr('data-x', xposition);
+        $(div).attr('data-y', yposition);
+        $(div).css('left', xposition);
+        $(div).css('top', yposition);
+        $(div).click(function() {
+            displayForm(this)
+        });
         $(treeGround).append(div);
         var center = $('<center>').appendTo(div);
         var pic = $('<img>').attr('src', 'images/profile.png');
@@ -117,5 +175,4 @@ var memberPic = '';
             reader.readAsDataURL(files[0]);
         }
     }
-
 }(jQuery));
